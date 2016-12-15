fece60fc71eeabbdfb26cb7b14680a782ebd8cea
Dolan <dolan_miu@hotmail.com>
2016-12-15 23:28:07 +0000

    turn off pump script

---
+++ py/turn-off-pump.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_Stepper
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+
+import time
+
+# create a default object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT()
+
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+turnOffMotors()
